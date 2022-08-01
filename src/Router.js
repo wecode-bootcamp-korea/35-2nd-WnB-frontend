c81f30e2409710d869807854688dd5ced681cc90
Yelihi <Yelihi19@gmail.com>
2022-08-01 16:43:48 +0900

    first commit: 초기 셋팅 중

---
+++ src/Router.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Nav from "./components/Nav/Nav";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
